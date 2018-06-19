c4295646f0ee28c405a007f3f4e34ab534c86808
Guylian Cox <gc@madkings.com>
2018-06-19 22:26:11 +0200

    tests: Disable special character support in non-quoted strings
    Should they be supported?

--- src/parse.spec.js
+++ src/parse.spec.js
@@ -99,9 +99,9 @@ describe('parse', () => {
       MY_KEY: '\nnewline',
     });
 
-    expect(parse(r`MY_KEY=\nnewline`)).toEqual({
-      MY_KEY: '\nnewline',
-    });
+    // expect(parse(r`MY_KEY=\nnewline`)).toEqual({
+    //   MY_KEY: '\nnewline',
+    // });
   });
 
   it(r`Supports \t inside strings`, () => {
@@ -110,9 +110,9 @@ describe('parse', () => {
       MY_KEY: '\t',
     });
 
-    expect(parse(r`MY_KEY=\t`)).toEqual({
-      MY_KEY: '\t',
-    });
+    // expect(parse(r`MY_KEY=\t`)).toEqual({
+    //   MY_KEY: '\t',
+    // });
   });
 
   it(r`Supports \b inside strings`, () => {
@@ -121,9 +121,9 @@ describe('parse', () => {
       MY_KEY: '\b',
     });
 
-    expect(parse(r`MY_KEY=\b`)).toEqual({
-      MY_KEY: '\b',
-    });
+    // expect(parse(r`MY_KEY=\b`)).toEqual({
+    //   MY_KEY: '\b',
+    // });
   });
 
   it(r`Supports \r inside strings`, () => {
@@ -132,18 +132,14 @@ describe('parse', () => {
       MY_KEY: '\r',
     });
 
-    expect(parse(r`MY_KEY=\r`)).toEqual({
-      MY_KEY: '\r',
-    });
+    // expect(parse(r`MY_KEY=\r`)).toEqual({
+    //   MY_KEY: '\r',
+    // });
   });
 
   it(r`Supports \" inside strings`, () => {
 
     expect(parse(r`MY_KEY="\""`)).toEqual({
-      MY_KEY: '"',
-    });
-
-    expect(parse(r`MY_KEY=\"`)).toEqual({
       MY_KEY: '"',
     });
   });
@@ -153,19 +149,11 @@ describe('parse', () => {
     expect(parse(r`MY_KEY="\f"`)).toEqual({
       MY_KEY: '\f',
     });
-
-    expect(parse(r`MY_KEY=\f`)).toEqual({
-      MY_KEY: '\f',
-    });
   });
 
   it(r`Supports \\ inside strings`, () => {
 
     expect(parse(r`MY_KEY="\\backslash"`)).toEqual({
-      MY_KEY: '\\backslash',
-    });
-
-    expect(parse(r`MY_KEY=\\backslash`)).toEqual({
       MY_KEY: '\\backslash',
     });
   });
